1403292af5c74002349c2a865cf5cde8837762be
DaWG <danielwicaksanag@gmail.com>
2025-12-09 14:27:56 +0700

    Revert "Merge branch 'Base-Game-Nimonscooked' of https://github.com/Daniel-W-G/Tubes-OOP into Base-Game-Nimonscooked"
    This reverts commit c52215c3bfd56943934c94dec0420106825d2acc, reversing
    changes made to 8ac1997e8ab68af05bd6b3f13ab3fa4519533a31.

--- nimonscooked/src/main/java/id/ac/itb/if2010/model/ChefAction.java
+++ nimonscooked/src/main/java/id/ac/itb/if2010/model/ChefAction.java
@@ -1,13 +1,8 @@
 package id.ac.itb.if2010.model;
 
 public enum ChefAction {
-    MOVE_UP,
-    MOVE_DOWN,
-    MOVE_LEFT,
-    MOVE_RIGHT,
-    INTERACT,
-    HOLD,
-    USE,
-    SERVE,
-    TRASH
+    IDLE,
+    MOVING,
+    BUSY_CUTTING,
+    BUSY_WASHING
 }
--- nimonscooked/src/main/java/id/ac/itb/if2010/model/IngredientState.java
+++ nimonscooked/src/main/java/id/ac/itb/if2010/model/IngredientState.java
@@ -1,9 +1,8 @@
 package id.ac.itb.if2010.model;
 
 public enum IngredientState {
-    RAW,        // Mentah
-    CHOPPED,    // Sudah dipotong
-    COOKING,    // Sedang dimasak 
-    COOKED,     // Sudah dimasak
-    BURNED      // Gosong
+    RAW,
+    CHOPPED,
+    COOKED,
+    BURNED
 }
